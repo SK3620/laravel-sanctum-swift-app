7e6bc863705afbc0f47fa78692849975be4d10ad
SK3620 <k.n.t11193620@gmail.com>
2024-08-05 11:26:12 +0900

    Initial Commit

---
+++ laravel-sanctum-swift-app/laravel_sanctum_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  laravel_sanctum_swift_appApp.swift
+//  laravel-sanctum-swift-app
+//
+//  Created by 鈴木 健太 on 2024/08/05.
+//
+
+import SwiftUI
+
+@main
+struct laravel_sanctum_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
